98a25a84f1f0fe6b15df1aca5239c13cfb38d361
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2024-03-24 17:13:40 -0300

    Gestão de tasks funcionando
    Não libera memória da pliha daas tasks

--- makefile
+++ makefile
@@ -1,9 +1,21 @@
-CFLAGS= -Wall -std=c99 -g
+CFLAGS= -Wall -Wextra -std=c99 -g
+
+all: testes/ping-pong-tasks1 testes/ping-pong-tasks2 testes/ping-pong-tasks3
+
+debug: CFLAGS+= -DDEBUG
+debug: all
+
+testes/ping-pong-tasks%: testes/ping-pong-tasks%.c ppos_core.c
+	gcc $(CFLAGS) $^ -o $@
+
 queue: queue.c queue.h
 	gcc $(CFLAGS) $< -o $@
 
 testafila: testafila.c queue.c
 	gcc $(CFLAGS) $^ -o $@
 
+ppos_core.o: ppos_core.c ppos.h ppos_data.h
+	gcc $(CFLAGS) -c ppos_core.c -o $@
+
 clean:
-	rm -rf testafila queue
+	rm -rf testafila queue testes/ping-pong-tasks{1..3}
--- ppos_core.c
+++ ppos_core.c
@@ -3,18 +3,20 @@
 #include "ppos.h"
 
 #define PPOS_TASK_ERROR_CODE -1
-#define PPOS_STACK_SZ 1024*64
-#define PPOS_DEBUG(msg, ...) printf("PPOS[%s]: "msg, __func__, __VA_ARGS__)
+#define PPOS_TASK_OK_CODE 0
+#define PPOS_STACK_SZ 1<<14
+#define PPOS_DEBUG(msg, ...) printf("PPOS[%s]: "msg"\n", __func__, __VA_ARGS__)
 
+task_t __TaskMain ;
 task_t *TaskCurr ;
-task_t *TaskMain ;
+task_t *TaskMain = &__TaskMain ;
 int idCounter = 0 ;
 
 void ppos_init () {
 #if DEBUG
     PPOS_DEBUG("%s", "Inicializando PpOS\n") ;
 #endif
-    TaskMain = TaskCurr ;
+    TaskCurr = TaskMain ;
     setvbuf (stdout, 0, _IONBF, 0) ;
     getcontext(&TaskCurr->context) ;
     TaskCurr->id = idCounter++ ;
@@ -39,7 +41,7 @@ int task_init (task_t *task,			// descritor da nova tarefa
     task->context.uc_stack.ss_size = PPOS_STACK_SZ ;
     task->context.uc_stack.ss_flags = 0 ;
     task->context.uc_link = 0 ;
-    makecontext(&task->context, start_func, 1, arg) ;
+    makecontext(&task->context, (void (*)(void))start_func, 1, arg) ;
 
     task->id = idCounter++ ;
 
@@ -54,12 +56,36 @@ int task_id () {
 }
 
 void task_exit (int exit_code) {
-    
+    (void)exit_code ;
+#ifdef DEBUG
+    PPOS_DEBUG("Saindo task %d.", task_id()) ;
+#endif
+    if (TaskCurr == TaskMain){
+#ifdef DEBUG
+        PPOS_DEBUG("%s", "Saindo Main.") ;
+#endif
+        exit(exit_code) ;
+    }
+#ifdef DEBUG
+    PPOS_DEBUG("%s", "Voltando para task main.") ;
+#endif
+    //free(TaskCurr->context.uc_stack.ss_sp) ;
+    task_switch(TaskMain) ;
 }
 
 int task_switch (task_t *task) {
-    if (task == NULL || task->context == NULL)
+    task_t *aux ;
+    if (task == NULL)
         return PPOS_TASK_ERROR_CODE ;
 
-    return swapcontext(&TaskCurr->context, &task->context) ;
+    aux = TaskCurr ;
+#ifdef DEBUG
+    PPOS_DEBUG("Trocando tasks %d -> %d", task_id(), task->id) ;
+#endif
+
+    TaskCurr = task ;
+    swapcontext(&aux->context, &task->context) ;
+    TaskCurr = aux ;
+
+    return PPOS_TASK_OK_CODE ;
 }
